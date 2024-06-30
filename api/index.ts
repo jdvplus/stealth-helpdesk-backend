d3e86bdf8e185612484f556763fc9ebcecb60a62
Jaime de Venecia <deveneciaj@gmail.com>
2024-06-30 12:10:03 -0700

    decrease explicit typing in api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,31 +1,15 @@
-import express, {
-  Request,
-  Response,
-  ErrorRequestHandler,
-  NextFunction,
-} from 'express';
+import express, { ErrorRequestHandler } from 'express';
 
 const app = express();
-const PORT: number = 3000;
+const PORT = 3000;
 
 app.use(express.json());
 
-app.get(
-  '/',
-  (req: Request, res: Response): Response => res.send('hello world')
-);
+app.get('/', (req, res) => res.send('hello world'));
 
-app.use(
-  (req: Request, res: Response): Response =>
-    res.status(404).send('oops! nothing here.')
-);
+app.use((req, res) => res.status(404).send('oops! nothing here.'));
 
-const globalErrorHandler: ErrorRequestHandler = (
-  err,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('something broke!');
 };
