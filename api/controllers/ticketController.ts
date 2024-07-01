ed34b9d1a38ef3305f0ceffd3035cd849974a6c5
Jaime de Venecia <deveneciaj@gmail.com>
2024-07-01 16:26:49 -0700

    refactor ticketController for clarity

--- api/controllers/ticketController.ts
+++ api/controllers/ticketController.ts
@@ -3,60 +3,53 @@ import path from 'path';
 
 import { Ticket, TicketController } from '../types';
 
+const db = path.join(__dirname, '../db.json');
+
 const ticketController: TicketController = {
   // retrieve all existing tickets from database
   getAllTickets: (req, res, next) => {
-    fs.readFile(
-      path.join(__dirname, '../db.json'),
-      'utf-8',
-      (err, data: string) => {
-        if (err) next(err);
+    fs.readFile(db, 'utf-8', (err, data: string) => {
+      if (err) next(err);
 
-        // handle if no tickets in database
-        const parsedData: Ticket[] = JSON.parse(data);
-        if (!parsedData.length)
-          res.locals.tickets = 'No tickets submitted yet!';
-        else res.locals.tickets = parsedData;
+      const parsedData: Ticket[] = JSON.parse(data);
+      res.locals.tickets = parsedData.length
+        ? parsedData
+        : 'No tickets submitted yet!';
 
-        next();
-      }
-    );
+      next();
+    });
   },
 
   // add a new ticket to database
   submitTicket: (req, res, next) => {
     const { name, email, description } = req.body;
 
-    fs.readFile(
-      path.join(__dirname, '../db.json'),
-      'utf-8',
-      (err, data: string) => {
-        if (err) next(err);
+    fs.readFile(db, 'utf-8', (err, data: string) => {
+      if (err) next(err);
 
-        const parsedData: Ticket[] = JSON.parse(data);
-        const { ticketId }: Ticket = parsedData[parsedData.length - 1];
-        const newTicketId: number = ticketId + 1;
-        const newTicket: Ticket = {
-          ticketId: newTicketId,
-          name,
-          email,
-          description,
-          status: 'new',
-          supportTeamResponse: '',
-        };
-        parsedData.push(newTicket);
+      const parsedData: Ticket[] = JSON.parse(data);
+      const { ticketId }: Ticket = parsedData[parsedData.length - 1];
+      const newTicketId: number = ticketId + 1;
+      const newTicket: Ticket = {
+        ticketId: newTicketId,
+        name,
+        email,
+        description,
+        status: 'new',
+        supportTeamResponse: '',
+      };
+      parsedData.push(newTicket);
 
-        fs.writeFile(
-          path.join(__dirname, '../db.json'),
-          JSON.stringify(parsedData),
-          'utf-8',
-          (err) => {
-            if (err) next(err);
-            next();
-          }
-        );
-      }
-    );
+      fs.writeFile(
+        path.join(__dirname, '../db.json'),
+        JSON.stringify(parsedData),
+        'utf-8',
+        (err) => {
+          if (err) next(err);
+          next();
+        }
+      );
+    });
   },
 };
 
