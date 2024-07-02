6fbe6e7e9ba5f6ea4eca9570d59278af8a6a852e
Jaime de Venecia <deveneciaj@gmail.com>
2024-07-02 13:36:41 -0700

    restore app to pre-vercel state; explore heroku

--- api/controllers/ticketController.ts
+++ api/controllers/ticketController.ts
@@ -8,7 +8,7 @@ import {
   SupportTeamResponse,
 } from '../types';
 
-const db = '/tmp/db.json';
+const db = path.join(__dirname, '../db.json');
 
 /**
  * Helper function to update the status of an existing ticket in the database.
