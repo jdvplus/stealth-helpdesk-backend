800469f7ffd0d70959f3c5db743c045919eea7d8
Jaime de Venecia <deveneciaj@gmail.com>
2024-07-02 14:31:49 -0700

    create Ticket model

---
+++ api/models/ticketModel.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const ticketSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  description: { type: String, required: true },
+  status: { type: String, default: 'new' },
+  supportTeamResponse: { type: String, default: '' },
+});
+export const Ticket = mongoose.model('ticket', ticketSchema);
