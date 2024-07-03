f9128664a5bfd33af736e7f4502b83b587a5dbe0
Jaime de Venecia <deveneciaj@gmail.com>
2024-07-03 16:41:01 -0700

    fix cors syntax error

--- api/index.ts
+++ api/index.ts
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 app.use(mongoSanitize());
 
 // GET route: retrieve all tickets from db
